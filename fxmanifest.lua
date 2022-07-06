029370c50c89946882fce6e056e8d1a67ac1facf
Jim Shield <gothicjim@gmail.com>
2022-07-06 10:21:14 +0100

    Version Bump

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,10 +1,11 @@
 name "Jim-Recycle"
 author "Jimathy"
+version "v2.0"
 description "Recycling Script By Jimathy"
 fx_version "cerulean"
 game "gta5"
 
 dependencies { 'qb-menu', 'qb-target', }
 shared_scripts { 'config.lua' }
-client_scripts { 'client.lua', 'dumpsters.lua' }
+client_scripts { 'dumpsters.lua' }
 server_script { 'server.lua' }
