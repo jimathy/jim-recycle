5658e9f5d4e6d1b211f2fd15939cc63ceab6816f
Jim Shield <gothicjim@gmail.com>
2022-07-06 22:16:31 +0100

    Never used PolyZone :sadge:

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,14 +7,6 @@ game "gta5"
 
 dependencies { 'qb-menu', 'qb-target', }
 
-client_scripts {
-    '@PolyZone/client.lua',
-    '@PolyZone/BoxZone.lua',
-    '@PolyZone/EntityZone.lua',
-    '@PolyZone/CircleZone.lua',
-    '@PolyZone/ComboZone.lua',
-	'client/*.lua',
-}
-
+client_scripts { 'client/*.lua', }
 server_scripts { 'server/*.lua' }
 shared_scripts { 'config.lua', }
