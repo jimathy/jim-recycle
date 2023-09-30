3d092d11fb1cf4bd861b2cc708718a17d596f095
zackSoftware <143184330+zackSoftware@users.noreply.github.com>
2023-09-30 22:28:32 +0100

    <fix>: Inventory inaccessible bug
    Fixed a bug where the inventory was not accessible anymore after searching an unavailable dumpster

--- client/dumpsters.lua
+++ client/dumpsters.lua
@@ -39,7 +39,11 @@ if Config.DumpsterDiving.Enable then if Config.Debug then print("^5Debug^7: ^2Lo
         if Config.Debug then print("^5Debug^7: ^2Starting Search of entity^7: '^6"..data.entity.."^7'") end
         for i = 1, #Config.DumpsterDiving.searched do
             if Config.DumpsterDiving.searched[i] == data.entity then
-                triggerNotify(nil, Loc[Config.Lan].error["searched"], "error") searched = true Searching = false return
+                triggerNotify(nil, Loc[Config.Lan].error["searched"], "error") 
+                searched = true 
+                Searching = false 
+                lockInv(false)
+                return
             end
         end
         if not searched then -- If hasn't been searched yet
